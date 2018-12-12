81f35ad779a9a102d72687316ef6f7f710646a2a
Dexter245 <Dexter245@googlemail.com>
2018-12-12 13:31:52 +0100

    project setup

---
+++ desktop/src/com/polymorphic_dissociation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.polymorphic_dissociation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.polymorphic_dissociation.treeGen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new treeGen(), config);
+	}
+}
